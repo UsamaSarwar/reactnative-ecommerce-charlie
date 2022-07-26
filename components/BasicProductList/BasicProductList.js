212ef7825efbcf6e399365d5d16037a1539f20ce
bukhtyar <bukhtyar.haider1@gmail.com>
2022-07-26 17:22:33 +0500

    add Checkout Screen and add screenshots

---
+++ components/BasicProductList/BasicProductList.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "../../constants";
+import { Ionicons } from "@expo/vector-icons";
+
+const BasicProductList = ({ title, price, size }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <View style={styles.IconContainer}>
+          <Ionicons name="square" size={30} color={colors.muted} />
+        </View>
+        <View style={styles.productInfoContainer}>
+          <Text style={styles.secondaryText}>{title}</Text>
+          <Text>{size}</Text>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.primaryText}>{price}$</Text>
+      </View>
+    </View>
+  );
+};
+
+export default BasicProductList;
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: colors.white,
+    height: 70,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.light,
+    padding: 5,
+  },
+  innerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  productInfoContainer: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    marginLeft: 10,
+  },
+  IconContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.light,
+    height: 40,
+    width: 40,
+    borderRadius: 5,
+  },
+  primaryText: {
+    fontSize: 15,
+    fontWeight: "600",
+    color: colors.primary,
+  },
+  secondaryText: {
+    fontSize: 15,
+    fontWeight: "600",
+  },
+});
