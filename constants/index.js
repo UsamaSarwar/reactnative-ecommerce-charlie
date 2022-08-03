754992143b2121b6246c18590b43e836bb96c154
bukhtyar <bukhtyar.haider1@gmail.com>
2022-08-03 15:26:45 +0500

    add Screenshots

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,2 @@
-export { default as colors } from "./Colors";
+export { default as colors } from "./colors";
 export { default as network } from "./network";
--- constants/network.js
+++ constants/network.js
@@ -1,6 +1,3 @@
 export default network = {
-  // serverip: "http://192.168.182.173:3000",
-  // serverip: "http://192.168.1.6:3000",
-  serverip: "http://192.168.18.178:3000",
-  
+  serverip: "http://192.168.182.173:3000",
 };
